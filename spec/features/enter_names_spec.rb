80486ffaeb7b6d0545bc4b4db8e70218b4550eb2
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-04 22:05:25 +0100

    View refactoring 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,6 +5,6 @@ require 'spec_helper'
 feature 'Player adds their name' do
 	scenario 'with valid name' do
 		sign_in_and_play
-		expect(page).to have_content('Ina vs. Jonny')
+		expect(page).to have_content('Ina vs Jonny')
 	end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 feature 'View hit points' do
   scenario 'see player 2 hit points' do
     sign_in_and_play
-    expect(page).to have_content('Jonny: 60hp')
+    expect(page).to have_content('Jonny: 60 hp')
   end
 end
