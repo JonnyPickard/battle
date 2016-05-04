d08a3ce98ba0f6a547a2628a66d1a2431576f93a
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-04 21:37:34 +0100

    Further refactor of views

--- app.rb
+++ app.rb
@@ -10,39 +10,42 @@ class Battle < Sinatra::Base
   enable :sessions
 
   get '/' do
-    erb :'index', :layout => :'layout'
+    erb :'new_warriors', :layout => :'home'
   end
 
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    session[:game] = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @player_1_hitpoints = $game.player_1.hitpoints
-    @player_2_hitpoints = $game.player_2.hitpoints
+    game = session[:game]
+    @player_1_name = game.player_1.name
+    @player_2_name = game.player_2.name
+    @player_1_hitpoints = game.player_1.hitpoints
+    @player_2_hitpoints = game.player_2.hitpoints
     erb :play, :layout => :'layout'
   end
 
   get '/attack' do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    $game.attack($game.player_2)
-    @player_1_hitpoints = $game.player_1.hitpoints
-    @player_2_hitpoints = $game.player_2.hitpoints
-    erb(:attack, :layout => :'layout')
+    game = session[:game]
+    @player_1_name = game.player_1.name
+    @player_2_name = game.player_2.name
+    game.attack(game.player_2)
+    @player_1_hitpoints = game.player_1.hitpoints
+    @player_2_hitpoints = game.player_2.hitpoints
+    erb :attack, :layout => :'layout'
   end
 
   get '/play2' do
-    @player_1_name = $game.player_1.name
-    @player_2_name = $game.player_2.name
-    @player_1_hitpoints = $game.player_1.hitpoints
-    @player_2_hitpoints = $game.player_2.hitpoints
-    erb(:play2, :layout => :'layout')
+    game = session[:game]
+    @player_1_name = game.player_1.name
+    @player_2_name = game.player_2.name
+    @player_1_hitpoints = game.player_1.hitpoints
+    @player_2_hitpoints = game.player_2.hitpoints
+    erb :play2, :layout => :'layout'
   end
 
   run! if app_file == $0
