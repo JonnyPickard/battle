0e8801de3771fcd30d818a2d62739b82f8d9a1f2
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-05 16:40:58 +0100

    Add unit spec for game and player classes

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -2,32 +2,41 @@ require 'game'
 
 describe Game do
 
-  subject(:game) {described_class.new(player_1, player_2)}
-  let(:player_1) {double(:player_1)}
-  let(:player_2) {double(:player_2)}
+  let(:player_1) { double(:player_1) }
+  let(:player_2) { double(:player_2) }
+
+  subject(:game) { described_class.new(player_1, player_2) }
 
   describe '#player_1' do
-    it 'returns player 1 after initialize' do
+    it 'Returns player 1 after initialize' do
       expect(game.player_1).to eq player_1
     end
   end
 
   describe '#player_2' do
-    it 'returns player 2 after initialize' do
+    it 'Returns player 2 after initialize' do
       expect(game.player_2).to eq player_2
     end
   end
 
   describe '#attack' do
-    it 'calls reduce_hitpoints on player' do
+    it 'Calls reduce_hitpoints on player' do
       expect(player_1).to receive(:reduce_hitpoints)
       game.attack(player_1)
     end
   end
 
-  describe '#turn_switcher' do
-    it 'switches turns after attack' do
+  describe "#winner" do
+    it "Correctly identifies a player 1 has won" do
+      allow(player_2).to receive(:dead?) { true }
+      allow(player_1).to receive(:dead?) { false }
+      expect(game.winner).to eq(player_1)
+    end
 
+    it "Correctly identifies a player 2 has won" do
+      allow(player_1).to receive(:dead?) { true }
+      allow(player_2).to receive(:dead?) { false }
+      expect(game.winner).to eq(player_2)
     end
   end
 end
--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -2,28 +2,28 @@ require 'player'
 
 describe Player do
 
-  subject(:jonny) {described_class.new('jonny')}
+  subject(:jonny) {described_class.new('Jonny')}
 
   describe '#name' do
-    it 'returns the name' do
-      expect(jonny.name).to eq 'jonny'
+    it 'Returns the name' do
+      expect(jonny.name).to eq 'Jonny'
     end
   end
 
   describe '#hitpoints' do
-    it 'returns the hitpoints' do
+    it 'Returns the hitpoints' do
       expect(jonny.hitpoints).to eq 60
     end
   end
 
   describe '#reduce_hitpoints' do
-    it 'reduces the hitpoints by 10' do
+    it 'Reduces the hitpoints by 10' do
       expect{jonny.reduce_hitpoints}.to change{jonny.hitpoints}.by(-10)
     end
   end
 
   describe '#dead?' do
-    it 'correctly identifies when a play has 0 or less hitpoints' do
+    it 'Correctly identifies when a play has 0 or less hitpoints' do
       jonny.hitpoints = 0
       expect(jonny.dead?).to be true
     end
